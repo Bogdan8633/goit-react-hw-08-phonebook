3be11fc949a5aee174b439cef91dbcc284a798ef
Bogdan <bogdanberezka@gmail.com>
2023-03-08 15:56:21 +0200

    add-totalContainer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 import MyContactsPage from './pages/MyContactsPage/MyContactsPage';
+import styles from './app.module.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <MyContactsPage />
+      <div className={styles.totalContainer}>
+        <MyContactsPage />
+      </div>
     </Provider>
   );
 }
