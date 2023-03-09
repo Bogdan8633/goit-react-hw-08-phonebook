6358659b9e0398a352a5199357ce6305e19445b9
Bogdan <bogdanberezka@gmail.com>
2023-03-09 14:22:34 +0200

    fix-bug-with-auth-state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './auth/auth-slice';
 
 import rootReducer from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
-  auth: authReducer,
 });
